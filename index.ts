bb2c06531d376bb77e965b272e3fc4dfbae2cdac
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2025-04-09 16:26:28 +0000

    Login

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
 import express from "express";
 import ejs from "ejs";
+import dotenv from "dotenv";
+import path, { format } from "path";
+import { connect } from "./database";
+import session from "./session";
+import { User } from "./interfaces/types";
+import { login } from "./database";
+
+//Import routers
 import loginRouter from "./routers/loginRouter";
 import registerRouter from "./routers/registerRouter";
 import homeRouter from "./routers/homeRouter";
@@ -15,8 +23,11 @@ import indexRouter from "./routers";
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
+app.use(session);
 
 //Routers
 app.use("/login", loginRouter());
@@ -31,6 +42,25 @@ app.use("/result", resultRouter());
 app.use("/suddendeath", suddendeathRouter());
 app.use("/", indexRouter());
 
-app.listen(app.get("port"), () =>
-  console.log("[server] http://localhost:" + app.get("port"))
-);
+app.post("/login", async(req, res) => {
+  const email : string = req.body.email;
+  const password : string = req.body.password;
+  try {
+      let user : User = await login(email, password);
+      delete user.password; 
+      req.session.user = user;
+      res.redirect("/")
+  } catch (e : any) {
+      res.redirect("/login");
+  }
+});
+
+app.listen(app.get("port"), async() => {
+  try {
+      await connect();
+      console.log("Server started on http://localhost:" + app.get('port'));
+  } catch (e) {
+      console.log(e);
+      process.exit(1); 
+  }
+});
--- routers/homeRouter.ts
+++ routers/homeRouter.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { secureMiddleware } from "../secureMiddleware";
 
 export default function homeRouter() {
     const router = express.Router();
 
-    router.get("/", (req, res) => {
-        res.render("homepage");
+    // router.get("/", (req, res) => {
+    //     res.render("homepage");
+    // });
+    router.get("/", async (req, res) => {
+        if (req.session.user) {
+            res.render("homepage", {user: req.session.user});
+        } else {
+            res.redirect("/login");
+        }
     });
+
+    router.get("/", secureMiddleware, async(req, res) => {
+        res.render("index");
+    });
+    
     return router;
 }
