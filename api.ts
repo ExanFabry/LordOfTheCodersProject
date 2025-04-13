044959be1707cbd5471f14477f70f72ead585b9b
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2025-04-13 18:44:51 +0000

    Fetch api

---
+++ api.ts
@@ -0,0 +1,39 @@
+import { Characters, Movies, Quotes } from "./interfaces/types";
+
+const quotesApi: string = "https://the-one-api.dev/v2/quote";
+let quotesArray: Quotes;
+
+const movieApi: string = "https://the-one-api.dev/v2/movie";
+let movieArray: Movies;
+
+const characterApi: string = "https://the-one-api.dev/v2/character";
+let characterArray: Characters;
+
+export async function getQuotes(){
+    const response = await fetch(quotesApi, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_KEY}`
+        }
+    });
+    quotesArray = await response.json();
+}
+
+export async function getMovies(){
+    const response = await fetch(movieApi, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_KEY}`
+        }
+    });
+    movieArray = await response.json();
+    console.log(movieArray);
+}
+
+export async function getCharacters(){
+    const response = await fetch(characterApi, {
+        headers: {
+            Authorization: `Bearer ${process.env.API_KEY}`
+        }
+    });
+    characterArray = await response.json();
+    console.log(characterArray);
+}
--- interfaces/types.ts
+++ interfaces/types.ts
@@ -16,3 +16,42 @@ export interface SessionData {
     user?: User;
     message?: FlashMessage;
 }
+
+export interface Quotes{
+    docs: {
+        _id:  ObjectId;
+        dialog:  string;
+        movie:  string;
+        character:  string;
+        id:  string;
+    }[];
+}
+
+export interface Movies{
+    docs: {
+        _id: ObjectId;
+        name: string;
+        runtimeInMinutes: number;
+        budgetInMillions: number;
+        boxOfficeRevenueInMillions: number;
+        academyAwardNominations: number;
+        academyAwardWins: number;
+        rottenTomatoesScore: number;
+    }[];
+}
+
+export interface Characters{
+    docs: {
+        _id: ObjectId;
+        name: string;
+        wikiUrl: string;
+        race: string;
+        birth: string;
+        gender: string;
+        death: string;
+        hair: string;
+        height: string;
+        realm: string;
+        spouse: string;
+    }[];
+}
