f11d34aad5cd5205aa85145e2600e931eb290251
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2025-05-22 13:30:28 +0000

    Add characters

--- routers/suddendeath.ts
+++ routers/suddendeath.ts
@@ -1,15 +1,29 @@
 //Imports
 import express from "express";
-import { getQuotes, getCharacters, getMovies, quotesArray } from "../api";
-import { quotes } from "./10-rounds";
+import { getQuotes, getCharacters, getMovies, quotesArray, characterArray } from "../api";
+// import { quotes } from "./10-rounds";
 import { client } from "../database";
-import { Quotes } from "../interfaces/types";
+import { Quotes, Characters } from "../interfaces/types";
+import { ObjectId } from "mongodb";
+
+export let quotesSuddenDeath: Quotes[] = [];
+let characters: {
+    id: ObjectId;
+    name: string | undefined;
+    correctCharacter: boolean;
+}[] = [];
+
+export let rightOrWrongCharacterSuddenDeath: boolean[] = [];
+
+let characterColorChange: number | undefined;
+let movieColorChange: number | undefined;
 
 //Sudden death router
 export default function suddendeathRouter() {
     const router = express.Router();
 
     router.get("/", async (req, res) => {
+        characters = [];
         //Haalt de blacklist quotes op.
         const resultBlackList = await client.db("Les").collection("blacklistQuotes").find<Quotes>({}).toArray();
         //Ervoor zorgen dat de api niet teveel wordt aangeroepen.
@@ -36,32 +50,67 @@ export default function suddendeathRouter() {
             randomQuote.character === undefined ||
             randomQuote.movie === undefined
         );
+        quotesSuddenDeath = [];
+        quotesSuddenDeath.push(randomQuote);
 
         //Als de gebruiker is ingelogd.
-        if (req.session.user) {
+        if (req.session.user) { 
             //Als req.session.rounds geen waarde heeft dan is het 1.
             if (!req.session.rounds) {
                 req.session.rounds = 1;
             }
+            //Steekt de characters in de array
+            generateCharacters(req);
             res.render("suddendeath", {
                 //Geeft de rondes mee
                 rounds: req.session.rounds,
                 //Geef de quotes mee
-                quotes: randomQuote
+                quotes: randomQuote,
+                //Geeft characters mee
+                characterOptions: characters,
+                characterColorChange: characterColorChange,
             });
         } else {
             res.redirect("/login");
         }
     });
     
-        //Verhoogt de rounds variabele.
-        router.post("/increase-rounds", (req, res) => {
+    //Verhoogt de rounds variabele.
+    router.post("/increase-rounds", (req, res) => {
+        if (req.session.user) {
+            if (!req.session.rounds) {
+                req.session.rounds = 1;
+            }
+            else{
+                req.session.rounds += 1;
+            }
+            res.redirect("/suddendeath");
+        } else {
+            res.redirect("/login");
+        }
+    });
+    
+        router.post("/add-character-points", (req, res) => {
             if (req.session.user) {
-                if (!req.session.rounds) {
-                    req.session.rounds = 1;
-                }
-                else{
-                    req.session.rounds += 1;
+                const characterValue = JSON.parse(req.body.characterOption);
+                const characterIndex = +(req.body.characterIndex as number);
+                characterColorChange = characterIndex;
+                if (characterValue.correctCharacter === false) {
+                    if (req.session.rounds !== undefined) {
+                        if (rightOrWrongCharacterSuddenDeath.length < req.session.rounds) {
+                            rightOrWrongCharacterSuddenDeath.push(false);
+                        } else {
+                            rightOrWrongCharacterSuddenDeath[req.session.rounds] = false;
+                        }
+                    }
+                } else if (characterValue.correctCharacter === true) {
+                    if (req.session.rounds !== undefined) {
+                        if (rightOrWrongCharacterSuddenDeath.length < req.session.rounds) {
+                            rightOrWrongCharacterSuddenDeath.push(true);
+                        } else { 
+                            rightOrWrongCharacterSuddenDeath[req.session.rounds] = true;
+                        }
+                    }
                 }
                 res.redirect("/suddendeath");
             } else {
@@ -69,4 +118,65 @@ export default function suddendeathRouter() {
             }
         });
     return router;
+}
+
+
+async function generateCharacters(req: express.Request) {
+    let randomCharacters: number[] = [];
+    let charactersPerRound: {
+        id: ObjectId;
+        name: string | undefined;
+        correctCharacter: boolean;
+    }[] = [];
+
+    let rightCharacterFind: Characters | undefined = characterArray.find((element) =>
+        new ObjectId(element._id).equals(new ObjectId(quotesSuddenDeath[0].character))
+    );
+    let rightCharacter: { 
+        id: ObjectId;
+        name: string | undefined;
+        correctCharacter: boolean;
+    } = {
+        id: new ObjectId(quotesSuddenDeath[0].character),
+        name: rightCharacterFind?.name,
+        correctCharacter: true,
+    };
+    if (rightCharacter !== undefined) {
+        charactersPerRound.push(rightCharacter);
+    }
+
+    for (let i: number = 0; i < 2; i++) {
+        let randomNumber = Math.floor(Math.random() * characterArray.length);
+        if (!randomCharacters.includes(randomNumber)) {
+            randomCharacters.push(randomNumber);
+        }
+
+        if (
+            characterArray[randomCharacters[i]] &&
+            characterArray[randomCharacters[i]].name &&
+            !charactersPerRound.some((character) => character.name === characterArray[randomNumber].name)
+        ) {
+            let newCharacter: {
+                id: ObjectId;
+                name: string | undefined;
+                correctCharacter: boolean;
+            } = {
+                id: characterArray[randomCharacters[i]]._id,
+                name: characterArray[randomCharacters[i]].name,
+                correctCharacter: false,
+            };
+            charactersPerRound.push(newCharacter);
+        } else {
+            i--;
+            randomCharacters.pop();
+        }
+    }
+
+    // Fisher-Yates shuffle
+    for (let k = charactersPerRound.length - 1; k > 0; k--) {
+        const j = Math.floor(Math.random() * (k + 1));
+        [charactersPerRound[k], charactersPerRound[j]] = [charactersPerRound[j], charactersPerRound[k]];
+    }
+
+    characters.push(...charactersPerRound.filter((m) => m !== undefined));
 }
